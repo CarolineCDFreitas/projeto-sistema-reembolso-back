c416f98c972dfe154a6604fe94e350bb6ceaef5e
CarolineCaetano <caroline_bdl@hotmail.com>
2025-05-08 16:03:25 -0300

    feat: integra SQLAlchemy e cria tabelas automaticamente

--- src/app.py
+++ src/app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from config import Config
 
+from src.model import db
+
 def create_app():
     app = Flask(__name__)
     
     app.config.from_object(Config)
+    
+    db.init_app(app)
+    
+    with app.app_context():
+        db.create_all()
     
     return app
