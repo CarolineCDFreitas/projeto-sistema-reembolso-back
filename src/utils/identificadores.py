2482724aa5b917b2b16b7f231d04f04414d5ee36
CarolineCaetano <caroline_bdl@hotmail.com>
2025-05-08 18:51:28 -0300

    feat: implementa funções para gerar e validar ID com TSID

--- src/utils/identificadores.py
+++ src/utils/identificadores.py
@@ -1,4 +1,25 @@
 from ulid import ULID
+from tsidpy import TSID
+
+from src.model import db
+from sqlalchemy import exists
+from src.model.reembolso_model import Reembolso
+
 
 def gerar_id_ulid():
     return str(ULID())
+
+
+def gerar_id_tsid(digito: int):
+    return TSID.create().number % (10**digito)
+
+
+def gerar_e_validar_unicidade_do_numero():
+    while True:
+        numero_gerado = gerar_id_tsid(7)
+        numero_existe = db.session.query(
+            exists().where(Reembolso.numero == numero_gerado)
+        ).scalar()
+
+        if not numero_existe:
+            return numero_gerado
