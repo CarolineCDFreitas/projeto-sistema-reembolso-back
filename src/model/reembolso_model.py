67e790638a611f3e0b4c52de212a596701eaa1dd
CarolineCaetano <caroline_bdl@hotmail.com>
2025-05-08 16:49:02 -0300

    feat: adiciona model Reembolso com relacionamento ao Colaborador. Utilizado CURDATE e NOW com text devido à versão 9.2.0 do MySQL em uso

---
+++ src/model/reembolso_model.py
@@ -0,0 +1,62 @@
+from src.model import db
+from sqlalchemy import text
+
+class Reembolso(db.Model):
+    __tablename__ = "reembolso"
+
+    id = db.Column(db.CHAR(26), primary_key=True)
+    numero = db.Column(db.Integer, nullable=False, unique=True)
+
+    id_colaborador = db.Column(
+        db.CHAR(26), db.ForeignKey("colaborador.id"), nullable=False
+    )
+
+    nome_solicitante = db.Column(db.String(255), nullable=False)
+    empresa = db.Column(db.String(50), nullable=False)
+    numero_prestacao = db.Column(db.String(50), nullable=False)
+    descricao = db.Column(db.String(255), nullable=False)
+
+    data = db.Column(db.Date, nullable=False, server_default=text("CURDATE()"))
+    tipo_reembolso = db.Column(db.String(50), nullable=False)
+    custo_centro = db.Column(db.String(20), nullable=False)
+    ordem_interna = db.Column(db.String(25), nullable=True)
+    divisao = db.Column(db.String(25), nullable=True)
+    pep = db.Column(db.String(25), nullable=True)
+    moeda = db.Column(db.String(25), nullable=False)
+    distancia_km = db.Column(db.Integer, nullable=True)
+    valor_km = db.Column(db.DECIMAL(10, 2), nullable=True)
+    valor_faturado = db.Column(db.DECIMAL(10, 2), nullable=False)
+    despesa = db.Column(db.DECIMAL(10, 2), nullable=False)
+
+    status = db.Column(
+        db.String(50), nullable=False, server_default=text('"em aberto"')
+    )
+    criado_em = db.Column(db.DateTime, nullable=False, server_default=text("NOW()"))
+
+    taxa_de_conversao = db.Column(db.String(25), nullable=True, default=None)
+    valor_convertido = db.Column(db.DECIMAL(10, 2), nullable=True, default=None)
+    moeda_de_conversao = db.Column(db.String(25), nullable=True, default=None)
+    hora_da_conversao = db.Column(db.DateTime, nullable=True, default=None)
+
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "numero": self.numero,
+            "id_colaborador": self.id_colaborador,
+            "empresa": self.empresa,
+            "numero_prestacao": self.numero_prestacao,
+            "descricao": self.descricao,
+            "data": self.data,
+            "tipo_reembolso": self.tipo_reembolso,
+            "custo_centro": self.custo_centro,
+            "ordem_interna": self.ordem_interna,
+            "divisao": self.divisao,
+            "pep": self.pep,
+            "moeda": self.moeda,
+            "distancia_km": self.distancia_km,
+            "valor_km": self.valor_km,
+            "valor_faturado": self.valor_faturado,
+            "despesa": self.despesa,
+            "status": self.status,
+            "criado_em": self.criado_em,
+        }
