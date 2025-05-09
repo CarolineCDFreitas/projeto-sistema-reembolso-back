7c914eb154cb8883fc1a578693fbed3313776904
CarolineCaetano <caroline_bdl@hotmail.com>
2025-05-09 18:43:58 -0300

    refactor: define todos os campos como obrigatórios com nullable=False

--- src/model/colaborador_model.py
+++ src/model/colaborador_model.py
@@ -2,12 +2,12 @@ from src.model import db
 
 class Colaborador(db.Model):
 
-    id = db.Column(db.CHAR(26), primary_key=True)
-    nome = db.Column(db.String(255))
-    email = db.Column(db.String(150))
-    senha = db.Column(db.String(255))
-    cargo = db.Column(db.String(100))
-    salario = db.Column(db.DECIMAL(10, 2))
+    id = db.Column(db.CHAR(26), primary_key=True, nullable=False)
+    nome = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    senha = db.Column(db.String(255), nullable=False)
+    cargo = db.Column(db.String(100), nullable=False)
+    salario = db.Column(db.DECIMAL(10, 2), nullable=False)
 
     def __init__(self, id, nome, email, senha, cargo, salario):
         self.id = id
