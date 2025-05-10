6e49f630e1194e8fff14dd9ecff01fbc98363ccf
CarolineCaetano <caroline_bdl@hotmail.com>
2025-05-10 20:32:55 -0300

    fix: adiciona try/except para entender erro do PostgreSQL

--- src/controller/reembolso_controller.py
+++ src/controller/reembolso_controller.py
@@ -13,36 +13,39 @@ bp_reembolso = Blueprint("reembolso", __name__, url_prefix="/reembolso")
 
 @bp_reembolso.route("/cadastrar", methods=["POST"])
 def cadastrar_solicitacao():
-    dados_requisicao = request.get_json()
-    novo_numero = gerar_e_validar_unicidade_do_numero()
+    try:
+        dados_requisicao = request.get_json()
+        novo_numero = gerar_e_validar_unicidade_do_numero()
 
-    dados_tratados = limpeza_de_dados(dados_requisicao)
+        dados_tratados = limpeza_de_dados(dados_requisicao)
 
-    nova_solicitacao = Reembolso(
-        id=gerar_id_ulid(),
-        numero=novo_numero,
-        id_colaborador="01JTST6X5M3JTPH9W0371RW82X",
-        nome_solicitante=dados_tratados.get("nomeCompleto"),
-        empresa=dados_tratados.get("empresa"),
-        numero_prestacao=dados_tratados.get("prestacaoDeContas"),
-        descricao=dados_tratados.get("descricaoMotivo"),
-        data=dados_tratados.get("data"),
-        tipo_reembolso=dados_tratados.get("tipoDeDespesa"),
-        custo_centro=dados_tratados.get("centro"),
-        ordem_interna=dados_tratados.get("ordemInterna"),
-        divisao=dados_tratados.get("divisao"),
-        pep=dados_tratados.get("pep"),
-        moeda=dados_tratados.get("moeda"),
-        distancia_km=dados_tratados.get("distKm"),
-        valor_km=dados_tratados.get("valorKm"),
-        valor_faturado=dados_tratados.get("valorFaturado"),
-        despesa=dados_tratados.get("despesaTotal"),
-    )
+        nova_solicitacao = Reembolso(
+            id=gerar_id_ulid(),
+            numero=novo_numero,
+            id_colaborador="01JTST6X5M3JTPH9W0371RW82X",
+            nome_solicitante=dados_tratados.get("nomeCompleto"),
+            empresa=dados_tratados.get("empresa"),
+            numero_prestacao=dados_tratados.get("prestacaoDeContas"),
+            descricao=dados_tratados.get("descricaoMotivo"),
+            data=dados_tratados.get("data"),
+            tipo_reembolso=dados_tratados.get("tipoDeDespesa"),
+            custo_centro=dados_tratados.get("centro"),
+            ordem_interna=dados_tratados.get("ordemInterna"),
+            divisao=dados_tratados.get("divisao"),
+            pep=dados_tratados.get("pep"),
+            moeda=dados_tratados.get("moeda"),
+            distancia_km=dados_tratados.get("distKm"),
+            valor_km=dados_tratados.get("valorKm"),
+            valor_faturado=dados_tratados.get("valorFaturado"),
+            despesa=dados_tratados.get("despesaTotal"),
+        )
 
-    db.session.add(nova_solicitacao)
-    db.session.commit()
+        db.session.add(nova_solicitacao)
+        db.session.commit()
 
-    return jsonify({"mensagem": "Solicitação cadastrada com sucesso!"}), 201
+        return jsonify({"mensagem": "Solicitação cadastrada com sucesso!"}), 201
+    except Exception as e:
+        return jsonify({"mensagem": f"Erro: ${str(2)}"}), 500
 
 
 @bp_reembolso.route("/todas-solicitacoes-em-aberto", methods=["GET"])
