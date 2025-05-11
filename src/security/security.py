c912718d29138436cd12e004be12a45a5f53c9a4
CarolineCaetano <caroline_bdl@hotmail.com>
2025-05-10 21:50:57 -0300

    fix: adiciona .decode() ao hash da senha para compatibilidade com PostgreSQL

--- src/security/security.py
+++ src/security/security.py
@@ -3,7 +3,7 @@ from src.security import bcrypt
 
 def hash_senha(senha):
     pw_hash = bcrypt.generate_password_hash(senha)
-    return pw_hash
+    return pw_hash.decode("utf-8")
 
 
 def checar_senha(senha, hash_senha):
