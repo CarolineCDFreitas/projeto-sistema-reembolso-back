4a6f4540a4bb147df4c4e7e66ebd1b217f2a0628
CarolineCaetano <caroline_bdl@hotmail.com>
2025-05-12 18:28:34 -0300

    feat: aumenta o tempo de expiração do token JWT

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 from os import environ
 from dotenv import load_dotenv
+from datetime import timedelta
 
 load_dotenv()
 
@@ -7,3 +8,5 @@ class Config():
     SQLALCHEMY_DATABASE_URI = environ.get("URL_DATABASE_PROD")
     SQLACLHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = environ.get("JWT_SECRET_KEY")
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
+    
